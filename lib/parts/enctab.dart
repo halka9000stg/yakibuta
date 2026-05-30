9e393187ada49d7d145dd23f8298ac884f2d1d20
halka9000stg <as.he.that.doth.serve@gmail.com>
2026-05-30 17:07:23 +0900

    ud: add desc for each encoding recodes

--- lib/parts/enctab.dart
+++ lib/parts/enctab.dart
@@ -17,12 +17,18 @@ class EncodingTab {
 
   bool hasKey(String key)
     => this._recs.any((EncodingTabRec r) => r.key == key);
-  void add(String key, Encoding encoding) {
+
+  void add(String key, Encoding encoding, {
+      String? group, String? series,
+      int? nr, String? variant, bool showable = true}) {
     if(this.hasKey(key)) {
       throw 0;
     }
-    this._recs.add((key: key, encoding: encoding));
-    this._recs.sort((EncodingTabRec a, EncodingTabRec b) => a.compareTo(b));
+    this._recs.add(etr(
+        key, encoding, group: group,
+        series: series, nr: nr, variant: variant,
+        showable: showable));
+    this._recs.sort();
   }
   Encoding search(String cand){
     Iterable<EncodingTabRec> t
@@ -37,43 +43,43 @@ class EncodingTab {
   List<String> list([bool useCounter = false])
     => this._recs.indexed
       .map<String>(((int, EncodingTabRec) r)
-        => (!useCounter ? "-" :  "${(r.$1 + 1).toString()}.") + " ${r.$2.key}: ${r.$2.encoding.name}")
+        => (!useCounter ? "-" :  "${(r.$1 + 1).toString()}.") + " ${r.$2.key}:\t${r.$2.encoding.name}" + (r.$2.showable ? "\t(${r.$2.shows})" : ""))
       .toList();
   String showList([bool useCounter = false]) => "\a\n" + this.list(useCounter).join("\n") + "\v\r\0";
   
   static EncodingTab get dflt {
     final t = EncodingTab();
     // Unicodes
-    t.add("u8", utf8);
-    t.add("u16", utf16);
-    t.add("u32", utf32);
+    t.add("u8", utf8, series: "UTF", group: "Unicode", nr: 8);
+    t.add("u16", utf16, series: "UTF", group: "Unicode", nr: 16);
+    t.add("u32", utf32, series: "UTF", group: "Unicode", nr: 32);
     // EUCs
-    t.add("euc", eucJp);
-    t.add("euck", eucKr);
+    t.add("euc", eucJp, series: "EUC", group: "Unix", variant: "JP");
+    t.add("euck", eucKr, series: "EUC", group: "Unix", variant: "KR");
     // CJKVs
-    t.add("sjis", shiftJis);
-    t.add("iso", iso2022jp);
-    t.add("big5", big5);
+    t.add("sjis", shiftJis, group: "CJKV", series: "JP");
+    t.add("iso", iso2022jp, series: "ISO 2022", group: "CJKV", variant: "JP");
+    t.add("big5", big5, group: "CJKV", series: "ZH");
     // ISO-8859s
-    t.add("lat1", latin1);
-    t.add("lat2", latin2);
-    t.add("lat3", latin3);
-    t.add("lat4", latin4);
-    t.add("lat5", latin5);
-    t.add("lat6", latin6);
-    t.add("lat7", latin7);
-    t.add("lat8", latin8);
-    t.add("lat9", latin9);
-    t.add("lat10", latin10);
-    t.add("latc", latinCyrillic);
-    t.add("lata", latinArabic);
-    t.add("latg", latinGreek);
-    t.add("lath", latinHebrew);
-    t.add("latt", latinThai);
+    t.add("lat1", latin1, series: "ISO 8859", group: "ISO Extended ASCII-like", nr: 1);
+    t.add("lat2", latin2, series: "ISO 8859", group: "ISO Extended ASCII-like", nr: 2);
+    t.add("lat3", latin3, series: "ISO 8859", group: "ISO Extended ASCII-like", nr: 3);
+    t.add("lat4", latin4, series: "ISO 8859", group: "ISO Extended ASCII-like", nr: 4);
+    t.add("lat5", latin5, series: "ISO 8859", group: "ISO Extended ASCII-like", nr: 5);
+    t.add("lat6", latin6, series: "ISO 8859", group: "ISO Extended ASCII-like", nr: 6);
+    t.add("lat7", latin7, series: "ISO 8859", group: "ISO Extended ASCII-like", nr: 7);
+    t.add("lat8", latin8, series: "ISO 8859", group: "ISO Extended ASCII-like", nr: 8);
+    t.add("lat9", latin9, series: "ISO 8859", group: "ISO Extended ASCII-like", nr: 9);
+    t.add("lat10", latin10, series: "ISO 8859", group: "ISO Extended ASCII-like", nr: 10);
+    t.add("latc", latinCyrillic, series: "ISO 8859", group: "ISO Extended ASCII-like", variant: "Cyrillic");
+    t.add("lata", latinArabic, series: "ISO 8859", group: "ISO Extended ASCII-like", variant: "Arabic");
+    t.add("latg", latinGreek, series: "ISO 8859", group: "ISO Extended ASCII-like", variant: "Greek");
+    t.add("lath", latinHebrew, series: "ISO 8859", group: "ISO Extended ASCII-like", variant: "Hebrew");
+    t.add("latt", latinThai, series: "ISO 8859", group: "ISO Extended ASCII-like", variant: "Thai");
     // Others
-    t.add("koi8r", Koi8rCodec());
-    t.add("koi8u", Koi8uCodec());
-    t.add("ascii", ascii);
+    t.add("koi8r", Koi8rCodec(), series: "KOI8", variant: "R");
+    t.add("koi8u", Koi8uCodec(), series: "KOI8", variant: "U");
+    t.add("ascii", ascii, series: "US America", showable: false);
     return t;
   }
 }
--- lib/parts/types.dart
+++ lib/parts/types.dart
@@ -1,8 +1,76 @@
 import "dart:convert";
 
-typedef EncodingTabRec = ({
-    String key,
-    Encoding encoding});
+class EncodingTabRec implements Comparable<EncodingTabRec> {
+  final String key;
+  final Encoding encoding;
+  final String? group;
+  final String? series;
+  final int? nr;
+  final String? variant;
+  final bool showable;
+  
+  EncodingTabRec({
+      required this.key,
+      required this.encoding,
+      this.group, this.series, this.nr, this.variant,
+      this.showable = true});
+  
+  
+  String get shows {
+    if(!this.showable) return "";
+    if(this.series == null || (this.nr == null && this.variant == null)) return "";
+    
+    String series_ = this.series!.replaceAll(" ", "-");
+    String nr = this.nr == null ? "" : "-${this.nr}";
+    String variant = this.variant == null ? "" : "-${this.variant}";
+    
+    return "$series_$nr$variant";
+  }
+  @override
+  int compareTo(EncodingTabRec other){
+    late int cnt;
+    // group > series > (nr | variant) > key > encoding.name
+    // non- null > null
+    cnt = this.group.compareTo(other.group);
+    if(cnt != 0) return cnt;
+    
+    cnt = this.series.compareTo(other.series);
+    if(cnt != 0) return cnt;
+    
+    cnt = this.group.compareTo(other.group);
+    if(cnt != 0) return cnt;
+    
+    cnt = this.nr.compareTo(other.nr);
+    if(cnt != 0) return cnt;
+    
+    cnt = this.variant.compareTo(other.variant);
+    if(cnt != 0) return cnt;
+    
+    return this.encoding.name.compareTo(other.encoding.name);
+  }
+}
+
+EncodingTabRec etr(String key, Encoding encoding, 
+    {String? group, String? series, int? nr, String? variant, bool showable = true})
+  => EncodingTabRec(
+    key: key, encoding: encoding,
+    group: group, series: series, nr: nr, variant: variant, showable: showable);
+
+extension ComparableExt<T extends Comparable<T>> on T? {
+  int compareTo(T? other){
+    T? self = this;
+    if(self == null && other == null) {
+      return 0;
+    } else if (self != null && other != null) {
+      return self.compareTo(other);
+    } else if (self == null) {
+      return 1;
+    } else {
+      return -1;
+    }
+  }
+}
+
 typedef Str2IntParser = Converter<String, List<int>>;
 typedef OnError<E, R> = R Function(E);
 
